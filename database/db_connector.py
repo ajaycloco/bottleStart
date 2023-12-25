ad82265cea6b8debbdccb9a46a7bae19a54eada5
Ajay Kumar Mahato <mahato.ajay@cloco.co.jp>
2023-12-25 12:05:34 +0545

    sql alchemy

--- database/db_connector.py
+++ database/db_connector.py
@@ -1,17 +1,40 @@
-import mysql.connector
+# db connection and connection session pool using sql alchemy
+from sqlalchemy import create_engine, URL, text
+from sqlalchemy.orm import sessionmaker
 
 
-def connect_to_db():
-    host = ''
-    user = 'root'
-    password = 'test'
-    database = 'artist_management'
+url = URL.create(
+    drivername='mysql+mysqlconnector',
+    host='localhost',
+    port=3306,
+    database='artist_management',
+    username='root',
+    password='test'
+)
 
-    connection = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
-    return connection
+engine = create_engine(url, echo=True)
+session_pool = sessionmaker(bind=engine)
+with session_pool() as session:
+    session.execute(text('SHOW tables;'))
+    session.commit()
+    session.rollback()
 
+
+# core db setup for python
+# import mysql.connector
+#
+#
+# def connect_to_db():
+#     host = ''
+#     user = 'root'
+#     password = 'test'
+#     database = 'artist_management'
+#
+#     connection = mysql.connector.connect(
+#         host=host,
+#         user=user,
+#         password=password,
+#         database=database
+#     )
+#     return connection
+#
